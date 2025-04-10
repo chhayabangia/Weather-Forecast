34ec5ee55bc22ceca88c3d0da5d8fa23935801d9
chhayabangia <chhayabangia@gmail.com>
2025-04-10 15:10:31 -0400

    updated files

--- server/dist/routes/api/weatherRoutes.js
+++ server/dist/routes/api/weatherRoutes.js
@@ -1,32 +1,27 @@
 import { Router } from 'express';
 const router = Router();
-
 import HistoryService from '../../service/historyService.js';
 import WeatherService from '../../service/weatherService.js';
-
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (req, res) => {
-  // TODO: GET weather data from city name
-  WeatherService.getWeatherForCity(req.body.cityName)
-  .then((data) => {
-    console.log('Line 13 route data', data);
-    res.json(data);
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json({ message: err.message });
-  });
-  // TODO: save city to search history
-  HistoryService.addCity(req.body.cityName)
-  .then((city) => {
-  res.json(city);
-  });
+    // TODO: GET weather data from city name
+    WeatherService.getWeatherForCity(req.body.cityName)
+        .then((data) => {
+        console.log('Line 13 route data', data);
+        res.json(data);
+    })
+        .catch((err) => {
+        console.log(err);
+        res.status(500).json({ message: err.message });
+    });
+    // TODO: save city to search history
+    HistoryService.addCity(req.body.cityName)
+        .then((city) => {
+        res.json(city);
+    });
 });
-
 // TODO: GET search history
 //router.get('/history', async (req, res) => {});
-
 // * BONUS TODO: DELETE city from search history
 //router.delete('/history/:id', async (req, res) => {});
-
 export default router;
